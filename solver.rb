e3ed63a3013b7f222dd81c92c86d786970380f4a
Edwin Gichuhi <gicheddy@gmail.com>
2023-11-17 13:14:13 +0300

    Add fizzbuzz checker method

--- solver.rb
+++ solver.rb
@@ -10,4 +10,16 @@ class Solver
   def self.reverse(word)
     word.reverse
   end
+
+  def self.fizzbuzz(n)
+    if n % 3 == 0 && n % 5 == 0
+      'fizzbuzz'
+    elsif n % 3 == 0
+      'fizz'
+    elsif n % 5 == 0
+      'buzz'
+    else
+      n.to_s
+    end
+  end
 end
