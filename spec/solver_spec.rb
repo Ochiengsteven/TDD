375406bc1362c7cb5d2d8aaaa720963557d5335c
Ochiengsteven <stevenochieng6305@gmail.com>
2023-11-17 12:16:28 +0300

    ADD FIZZBUZZ TEST

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -20,4 +20,22 @@ RSpec.describe Solver do
       expect(Solver.reverse('hello')).to eq('olleh')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'returns "fizz" for numbers divisible by 3' do
+      expect(Solver.fizzbuzz(9)).to eq('fizz')
+    end
+
+    it 'returns "buzz" for numbers divisible by 5' do
+      expect(Solver.fizzbuzz(10)).to eq('buzz')
+    end
+
+    it 'returns "fizzbuzz" for numbers divisible by both 3 and 5' do
+      expect(Solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+
+    it 'returns the number as a string for other cases' do
+      expect(Solver.fizzbuzz(7)).to eq('7')
+    end
+  end
 end
